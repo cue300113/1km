ff3a9064439cccfdc73481065caf370d260b3799
cue300113 <haotianma345@gmail.com>
2025-07-29 18:07:38 +0800

    Update API and WebSocket URLs to backend

--- src/api/chat.js
+++ src/api/chat.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://101.201.52.163:8081', // 阿里云后端地址
+  baseURL: 'https://101.201.52.163:443', // 阿里云后端地址
   timeout: 10000
 })
 
